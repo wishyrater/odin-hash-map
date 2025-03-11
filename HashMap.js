f0e3c59e87acc6a9797042904b4c9eac711dd8ef
Julian Andersen <julian.cesar.andersen@gmail.com>
2025-03-11 23:29:56 +0100

    add hash and set methods

---
+++ HashMap.js
@@ -0,0 +1,32 @@
+class HashMap {
+    constructor(loadFactor, capacity) {
+        this.loadFactor = loadFactor;
+        this.buckets = Array(capacity);
+    }
+
+    hash = (key) => {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+
+        return hashCode;
+    }
+
+    set = (key, value) => {
+        const index = this.hash(key) % this.buckets.length;
+
+        this.buckets[index] = [key, value];
+    }
+
+    printBuckets = () => {
+        console.log(this.buckets);
+    }
+}
+
+const test = new HashMap(0.75, 16);
+
+test.set('oliver', 'green');
+test.printBuckets();
